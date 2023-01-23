c631adfe002b068f06e3487a1deb48be5c928a92
ch1kilin <98619131+ch1kilin@users.noreply.github.com>
2023-01-23 13:18:54 -0600

    Add files via upload

---
+++ lab 2.7.py
@@ -0,0 +1,17 @@
+#angel de jesus rosas muñoz
+
+
+import ncclient
+from ncclient import manager
+
+m = manager.connect(
+    host="10.10.20.48",
+    port=830,
+    username="developer",
+    password="C1sco12345",
+    hostkey_verify=False
+)
+
+print("#Supported Capabilities (YANG models):")
+for capability in m.server_capabilities:
+    print(capability)
